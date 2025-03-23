123a76fbcdd569b7b33e0cff6892cdb2d4c5fe3b
NazerkeKenzhiyeva <159401694+NazerkeKenzhiyeva@users.noreply.github.com>
2025-03-23 21:48:31 +0500

    Delete Task1/Arrays directory

--- Task1/Arrays/b.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-arr = [int(i) for i in input().split()]
-for i in range(n):
-    if(arr[i]%2==0):
-        print(arr[i], end=" ")
--- Task1/Arrays/c.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-arr = [int(i) for i in input().split()]
-count = 0
-for i in arr:
-    if(i > 0):
-        count+=1
-print(count)
